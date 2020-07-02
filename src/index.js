1890ad628ce3e064fe67ba1bc9709b67e9205290
Clara Hernández <clara.herni@gmail.com>
2020-07-02 16:57:11 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import {
-  obtenerNombresPokemon,
-  obtenerInfoPokemon,
-} from './api.js'
-
-import {
-  agregarPokemonAPaginaGeneral,
-  cargarNombres,
-  mostrarPokemonIndividual,
+  cargarBuscador,
   actualizarFlechasPaginas,
-  actualizarFlechasPokemon
-} from './ui.js'
+  cargarPagina,
+} from './ui/pagina-general.js'
 
-const MAX_POKEMON = 807;
+const MAX_POKEMON = 964;
 
+function inicializar() {
+  cargarPagina(0);
+  cargarBuscador(); 
 
-async function cargarPaginaGeneral(desde = 0) {
-    document.querySelector('#pagina-general').innerHTML = '';
-    for(let i = 0; i < 20; i++){
-      agregarPokemonAPaginaGeneral(await obtenerInfoPokemon(desde + i + 1), actualizarPokemon);
-    }
-}
-
-async function actualizarPokemon(id) {
-    //mostrarCartelActualizacion();
-    const infoPokemon = await obtenerInfoPokemon(id);
-    mostrarPokemonIndividual(infoPokemon);
-}
-
-async function inicializar() {
-    const listaNombres = await obtenerNombresPokemon(MAX_POKEMON);
-    cargarNombres(listaNombres, actualizarPokemon); 
-    cargarPaginaGeneral(0);
-    actualizarFlechasPaginas(cargarPaginaGeneral);
-    actualizarFlechasPokemon(actualizarPokemon, MAX_POKEMON);
-    //actualizarPokemon();
+  actualizarFlechasPaginas();
 }
 
 inicializar();
---
+++ src/ui/datos-pokemon.js
@@ -0,0 +1,76 @@
+import {
+  obtenerInfoPokemon,
+} from '../api.js'
+
+
+
+function cargarStats(stats) {
+    const $listaStats = document.querySelector('#stats')
+    Object.keys(stats).forEach((index) => {
+        const $barra = document.querySelector(`#${stats[index].stat.name}`);
+        $barra.textContent = `${stats[index]['base_stat']}`
+        $barra.setAttribute('value', `${stats[index]['base_stat']}`);
+        document.querySelector(`#${stats[index].stat.name}-value`).textContent = `${stats[index]['base_stat']}`
+
+    });
+}
+
+function cargarImagen(infoPokemon) {
+    const $imgFrente = document.querySelector('#imagen-frente');
+    $imgFrente.setAttribute('src', infoPokemon.sprites['front_default']);
+    $imgFrente.setAttribute('alt', infoPokemon.name);
+}
+
+function cargarPesoyAltura(peso, altura) {
+    document.querySelector('#weight').textContent = `Weight: ${peso}`
+    document.querySelector('#height').textContent = `Height: ${altura}`
+}
+
+function cargarTipos(listaTipos) {
+  const $tipo = document.querySelector('#tipo');
+  $tipo.textContent = ''
+  Object.keys(listaTipos).forEach((index) => {
+      const $tipoNuevo = document.createElement('span');
+      $tipoNuevo.setAttribute('class', `tipos`);
+      $tipoNuevo.textContent = listaTipos[index].type.name;
+      $tipo.appendChild($tipoNuevo);
+  })
+}
+
+function cargarNombre (nombre) {
+    document.querySelector('#nombre').textContent = nombre
+}
+
+function cargarId(id){
+  document.querySelector('#id-pokemon').textContent = `#${id}`;
+}
+
+function mostrarPokemonIndividual(infoPokemon){
+    document.querySelector('#pagina-individual').classList.remove('oculto');
+    cargarNombre(infoPokemon.name);
+    cargarStats(infoPokemon.stats);
+    cargarImagen(infoPokemon);
+    cargarPesoyAltura(infoPokemon.weight, infoPokemon.height);
+    cargarTipos(infoPokemon.types);
+    cargarId(infoPokemon.id); 
+}
+
+export async function actualizarPokemon(referencia) {
+  mostrarCartelCarga();
+  const infoPokemon = await obtenerInfoPokemon(referencia);
+  mostrarPokemonIndividual(infoPokemon);
+  ocultarCartelCarga();
+}
+
+function mostrarCartelCarga(){
+  document.querySelector('#pagina-individual-cargando').textContent = 'Cargando...';
+  document.querySelector('#pagina-individual-cargando').classList.remove('oculto');
+
+  document.querySelector('#pagina-individual').classList.add('oculto');
+}
+
+function ocultarCartelCarga(){
+  document.querySelector('#pagina-individual').classList.remove('oculto');
+  document.querySelector('#pagina-individual-cargando').classList.add('oculto');
+
+}
